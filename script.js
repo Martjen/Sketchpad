eb933b302d3a0728b3bd2b0472e99fd4677d41e2
Martjen <martinjensen_94hotmail.com>
2015-05-23 10:39:21 +0200

    Fixed the highlighting

--- script.js
+++ script.js
@@ -7,15 +7,15 @@ $(document).ready(function(){
 
 		var answer = prompt('How many blocks?');
 
-		$('.divobj').hover(function() {
-			$(this).addClass('highlight');
-		});
-
 		for (var i = 0; i < answer; i++) {
 			$(document).ready(function() {
 				$('#container').append('<div class="divobj"></div>');
 			});
 		};
+
+		$('.divobj').hover(function() {
+			$(this).addClass('highlight');
+		});
 
 	};
 });
