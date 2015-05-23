e6d7b8e5587d8eaddfaa4ff1be968075c0b7ac7c
Martjen <martinjensen_94hotmail.com>
2015-05-23 10:58:36 +0200

    Final for now

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 $(document).ready(function(){
 	buttonClicked = function() {
 
+		//Deleting the previous grid.
 		$.when('buttonClicked').done(function() {
 			$('.divobj').remove();
 		});
 
-		var answer = prompt('How many blocks?');
+		//prompting user for number of divs.
+		var answer = prompt('How many blocks? (768 looks best)');
 
+		//Adding divs.
 		for (var i = 0; i < answer; i++) {
 			$(document).ready(function() {
 				$('#container').append('<div class="divobj"></div>');
 			});
 		};
 
+		//Adding a highlight when hovered over.
 		$('.divobj').hover(function() {
 			$(this).addClass('highlight');
 		});
